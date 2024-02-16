bd4cb0381ef6d31fe5147bc5a7748d974b9b60de
Dmitri Khassanov <khdmitri@yahoo.com>
2024-02-16 16:39:51 +0700

    v.0.3.3 yoga_telegram_backend #2 bug fix /api/v2

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = "/api/v2"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
