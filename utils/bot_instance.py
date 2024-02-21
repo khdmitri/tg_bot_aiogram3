5b15d1fe22930c68315850e0eb6cd565fdc11ee9
Dmitri Khassanov <khdmitri@yahoo.com>
2024-02-21 12:21:45 +0700

    v.0.3.4 fixes #2

--- utils/bot_instance.py
+++ utils/bot_instance.py
@@ -2,16 +2,18 @@ from aiogram import Bot
 
 
 class BotInstanceSingleton(object):
+    _bot_instance = None
+
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(BotInstanceSingleton, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
-        self._bot_instance = None
-
     def init_bot_instance(self, bot: Bot):
+        print("Current Bot Instance:", self._bot_instance)
         self._bot_instance = bot
+        print("=== Current Bot Instance:", self._bot_instance)
 
     def get_instance(self) -> Bot:
+        print("GET: Current Bot Instance:", self._bot_instance)
         return self._bot_instance
