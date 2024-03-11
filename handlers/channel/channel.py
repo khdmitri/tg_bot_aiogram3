5e9f118c93248fdbc763537999d6403f2658107c
Dmitri Khassanov <khdmitri@yahoo.com>
2024-03-11 10:06:45 +0700

    v.0.3.12 join channel approve and decline update #5

--- handlers/channel/channel.py
+++ handlers/channel/channel.py
@@ -41,13 +41,14 @@ async def check_access_right(update: types.ChatJoinRequest):
                         if chat_member and chat_member.status == ChatMemberStatus.MEMBER:
                             await update.approve()
                         else:
-                            await update.answer(text="К сожалению, Вы не подписаны на наш канал: https://t.me/yoga_master_mind.\n"+
-                                                "Подпишитесь на наш канал и попробуйте снова.")
+                            await bot.send_message(chat_id=user.id,
+                                                   text="К сожалению, Вы не подписаны на наш канал: https://t.me/yoga_master_mind.\n" +
+                                                        "Подпишитесь на наш канал и попробуйте снова.")
                             await update.decline()
                     except TelegramBadRequest:
-                        await update.answer(
-                            text="К сожалению, Вы не подписаны на наш канал: https://t.me/yoga_master_mind.\n" +
-                                 "Подпишитесь на наш канал и попробуйте снова.")
+                        await bot.send_message(chat_id=user.id,
+                                               text="К сожалению, Вы не подписаны на наш канал: https://t.me/yoga_master_mind.\n" +
+                                                    "Подпишитесь на наш канал и попробуйте снова.")
             else:
                 logger.warning("Practise is invalid")
                 await update.decline()
