f5a41a6bb1f914ce7c96bc057e4be8d6bb1aeaaa
Dmitri Khassanov <khdmitri@yahoo.com>
2024-03-19 15:43:35 +0700

    v.0.5.2 web payment crud_web_payment bug fixes #14

--- crud/crud_web_payment.py
+++ crud/crud_web_payment.py
@@ -1,10 +1,17 @@
+from typing import Any, Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from crud.base import CRUDBase
 from models.web_payment import WebPayment
 from schemas.web_payment import WebPaymentCreate, WebPaymentUpdate
 
 
 class CRUDWebPayment(CRUDBase[WebPayment, WebPaymentCreate, WebPaymentUpdate]):
-    pass
+    async def get(self, db: AsyncSession, id: Any) -> Optional[WebPayment]:
+        result = await db.execute(select(self.model).filter(self.model.payment_id == id))
+        return result.scalars().first()
 
 
 crud_web_payment = CRUDWebPayment(WebPayment)
